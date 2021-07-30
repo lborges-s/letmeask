d5f198b6cac467534aa1e41fc05ca8e36317829d
Lucas Borges de Souza <lucasb.souza01@hotmail.com>
2021-07-29 23:02:07 -0300

    Update room
    - Insert a new room
    - Create room page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,10 +6,13 @@ import googleIconImg from '../assets/images/google-icon.svg';
 
 import '../styles/auth.scss';
 import { useAuth } from '../hooks/useAuth';
+import { FormEvent, useState } from 'react';
+import { database } from '../services/firebase';
 
 export function Home() {
     const history = useHistory();
     const { user, signInWithGoogle } = useAuth();
+    const [roomCode, setRoomCode] = useState('');
 
     async function handleCreateRoom() {
         if (!user) {
@@ -18,6 +21,26 @@ export function Home() {
 
         history.push('/rooms/new');
     }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()) {
+            alert('Room does not exists.');
+            return;
+        }
+
+
+        history.push(`rooms/${roomCode}`)
+
+    }
+
     return (
         <div id="page-auth">
             <aside>
@@ -33,10 +56,12 @@ export function Home() {
                         Crie sua sala com o Google
                     </button>
                     <div className="separator">ou entre em uma sala</div>
-                    <form >
+                    <form onSubmit={handleJoinRoom}>
                         <input
                             type="text"
                             placeholder="Digite o código da sala"
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
                         />
                         <Button type="submit">
                             Entrar na sala
